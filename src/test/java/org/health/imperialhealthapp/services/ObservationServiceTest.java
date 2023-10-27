0809237a502907f4226e441fa9f777fd5033036d
dasunpubudumal <dasun.15@cse.mrt.ac.lk>
2023-10-27 09:39:20 +0100

    unit tests

--- src/test/java/org/health/imperialhealthapp/services/ObservationServiceTest.java
+++ src/test/java/org/health/imperialhealthapp/services/ObservationServiceTest.java
@@ -17,11 +17,12 @@ import org.springframework.http.ResponseEntity;
 import java.sql.Date;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class ObservationServiceTest {
 
@@ -32,6 +33,8 @@ class ObservationServiceTest {
     MeasurementTypeRepository measurementTypeRepository = mock(
             MeasurementTypeRepository.class
     );
+
+    String uuid = UUID.randomUUID().toString();
 
     ObservationService service;
 
@@ -48,6 +51,28 @@ class ObservationServiceTest {
                                         .build()
                         )
                 ));
+        when(repository.findById(any())).thenReturn(
+                Optional.ofNullable(Observation.builder()
+                        .id(UUID.fromString(uuid))
+                        .measurementType(MeasurementType.builder().measurementType("x").unit("y").build())
+                        .date(Date.valueOf("2022-01-03"))
+                        .patient(1)
+                        .value(10.2)
+                        .build())
+        );
+        when(measurementTypeRepository.findByMeasurementType(any()))
+                .thenReturn(
+                        Optional.of(MeasurementType.builder().measurementType("rate").unit("ss").build())
+                );
+        when(repository.save(any())).thenReturn(
+                Observation.builder()
+                        .id(UUID.fromString(uuid))
+                        .measurementType(MeasurementType.builder().measurementType("x").unit("y").build())
+                        .date(Date.valueOf("2022-01-03"))
+                        .patient(1)
+                        .value(10.2)
+                        .build()
+        );
         service = new ObservationService(repository, measurementTypeRepository);
     }
 
@@ -59,5 +84,55 @@ class ObservationServiceTest {
         );
         assertTrue(Objects.nonNull(generalResultResponseEntity.getBody()));
         assertEquals(generalResultResponseEntity.getBody().getData().getContent().size(), 1);
+    }
+
+    @Test
+    @DisplayName("Check unit tests for finding by ID")
+    void getById() {
+        ResponseEntity<GeneralResult<ObservationDto>> byId = service.findById(uuid);
+        assertTrue(Objects.nonNull(byId.getBody()));
+        assertEquals(
+                uuid,
+                byId.getBody().getData().getId()
+        );
+    }
+
+    @Test
+    @DisplayName("Check unit tests for save")
+    void save() {
+        try {
+            service.save(
+                    ObservationDto.builder()
+                            .id(uuid)
+                            .measurementType("heart-rate")
+                            .unit("beats/minute")
+                            .date("2023-09-10T08:54:33Z")
+                            .value(76.0d)
+                            .patient(101)
+                            .build()
+            );
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    @DisplayName("Check unit tests for update")
+    void update() {
+        try {
+            service.update(
+                    ObservationDto.builder()
+                            .id(uuid)
+                            .measurementType("heart-rate")
+                            .unit("beats/minute")
+                            .date("2023-09-10T08:54:33Z")
+                            .value(76.0d)
+                            .patient(101)
+                            .build(),
+                    uuid
+            );
+        } catch (Exception e) {
+            fail();
+        }
     }
 }
