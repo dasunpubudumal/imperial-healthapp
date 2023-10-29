f51208255b67ec94df61f64e51c5080e98b47709
dasunpubudumal <dasun.15@cse.mrt.ac.lk>
2023-10-29 10:30:31 +0000

    configs

--- src/main/java/org/health/imperialhealthapp/config/WebConfiguration.java
+++ src/main/java/org/health/imperialhealthapp/config/WebConfiguration.java
@@ -2,10 +2,8 @@ package org.health.imperialhealthapp.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@EnableWebMvc
 @EnableSpringDataWebSupport
 public class WebConfiguration {
 }
