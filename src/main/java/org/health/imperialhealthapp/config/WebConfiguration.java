2b0a2229a03b07fae94a40614fea3fd46712ad31
dasunpubudumal <dasun.15@cse.mrt.ac.lk>
2023-11-07 12:45:03 +0000

    HA: Enable txn management

--- src/main/java/org/health/imperialhealthapp/config/WebConfiguration.java
+++ src/main/java/org/health/imperialhealthapp/config/WebConfiguration.java
@@ -3,9 +3,11 @@ package org.health.imperialhealthapp.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableSpringDataWebSupport
+@EnableTransactionManagement
 @EnableMethodSecurity
 public class WebConfiguration {
 }
